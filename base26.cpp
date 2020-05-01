51963b3a0c7191756fe48c2221422af79f01d72d
Pawan <dsxv007@gmail.com>
2020-05-01 17:35:35 +0530

    1may

---
+++ base26.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define int long long
+
+vector<int> solve(int n ){
+	vector<int> v ;
+	while(n){
+		n-- ;
+		v.push_back(1 + n%26) ;
+		n -= n%26 ;
+		n /= 26 ;
+	}
+	reverse(v.begin(),v.end()) ;
+	return v ;
+}
+
+int32_t main(){
+	int n; 
+	cin >> n ;
+	auto v = solve(n) ;
+	for(auto i : v) cout << i << " " ;
+	return 0 ;
+}
+
